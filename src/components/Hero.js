9ba28094f9f92dd775a20fd0d2622d88afd3060b
Prakarsh333 <ayush.raghuvanshi@students.iiit.ac.in>
2024-10-29 21:10:38 +0530

    work experience

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Header() {
     <div class="text-white absolute inset-0 flex flex-col items-center justify-center translate-y-[0%]">
     <h1 class="text-7xl text-center mb-4 font-mono font-bold animate-bounce animate-delay-300 animate-infinite ">Hello, I'm</h1>
     <h2 class="uppercase bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text text-7xl mt-4 text-center font-calibre mb-4 animate-bounce animate-bounce animate-delay-100 animate-infinite ">Ayush Raghuvanshi</h2>
-    <h3 class="text-white/70 text-xl font-mono ">I am a Full Stack Developer and BlockChain Enthusiast</h3>
+    <h3 class="text-white/70 text-xl font-mono ">I am a Full Stack Developer and Electronics Enthusiast</h3>
     </div>
     </div>
         </>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-function Header() {
+function Work() {
     // const Expref = useRef<HTMLDivElement | null> (null);
     return (
         <>
@@ -167,32 +167,25 @@ function Header() {
     <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-800 to-pink-800 text-white">
     <section className="dark:bg-gray-100 dark:text-gray-800">
 	<div className="container mx-auto flex flex-col p-6">
-		<h2 className="py-4 text-3xl font-bold text-center">Temporibus elit</h2>
+		<h2 className="py-4 text-3xl font-bold text-center">Work Experience</h2>
 		<div className="divide-y dark:divide-gray-300">
 			<div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
 				<div className="flex items-center justify-center lg:col-span-1 col-span-full">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
-						<path d="M472,16H168a24,24,0,0,0-24,24V344a24,24,0,0,0,24,24H472a24,24,0,0,0,24-24V40A24,24,0,0,0,472,16Zm-8,320H176V48H464Z"></path>
-						<path d="M112,400V80H80V408a24,24,0,0,0,24,24H432V400Z"></path>
-						<path d="M48,464V144H16V472a24,24,0,0,0,24,24H368V464Z"></path>
-					</svg>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUQpc9i74M2yHZFFY9sUX6phUvEcTNSpnqwg&s" class="h-24, w-24"></img>
 				</div>
 				<div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
-					<span className="text-xs tracking-wider uppercase dark:text-violet-600">Step 1 - Nihil</span>
-					<span className="text-xl font-bold md:text-2xl">Veritatis dolores</span>
+					<span className="text-xs tracking-wider uppercase dark:text-violet-600">January 2024 - March 2024</span>
+					<span className="text-xl font-bold md:text-2xl">SDE Intern at One Immersive</span>
 					<span className="mt-4 dark:text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
 				</div>
 			</div>
 			<div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
 				<div className="flex items-center justify-center lg:col-span-1 col-span-full">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
-						<path d="M285.177,179l15.513-3.914-7.827-31.028-15.514,3.913a176.937,176.937,0,0,0-129.3,133.557l-3.407,15.633,31.266,6.814,3.406-15.634A145.559,145.559,0,0,1,285.177,179Z"></path>
-						<path d="M363.624,147.871C343.733,72.077,274.643,16,192.7,16,95.266,16,16,95.266,16,192.7c0,82.617,57,152.163,133.735,171.4A176.769,176.769,0,0,0,320.7,496c97.431,0,176.7-79.266,176.7-176.695C497.392,238.071,441.64,167.336,363.624,147.871ZM48,192.7C48,112.91,112.91,48,192.7,48s144.7,64.91,144.7,144.7-64.911,144.7-144.7,144.7S48,272.481,48,192.7ZM320.7,464c-60.931,0-115.21-38.854-135.843-94.792,2.6.115,5.214.184,7.843.184a176.862,176.862,0,0,0,32.7-3.047l97.625,97.625C322.247,463.983,321.473,464,320.7,464Zm41.528-6.083L260.26,355.954a176.9,176.9,0,0,0,43.662-26.072L408.37,434.33A144.385,144.385,0,0,1,362.223,457.917Zm69.3-45.692L326.851,307.557a177.082,177.082,0,0,0,27.911-44.5L457.67,365.964A144.661,144.661,0,0,1,431.519,412.225Zm33.594-84.073-99.42-99.42a176.785,176.785,0,0,0,3.7-36.036c0-3.285-.1-6.547-.276-9.787a145.054,145.054,0,0,1,96.276,136.4C465.392,322.276,465.291,325.224,465.113,328.152Z"></path>
-					</svg>
+          <img src="https://yt3.googleusercontent.com/t-LQtSEGx32XWawrXg27be00C0ILo2m4N495zkFECn5B81HXiClq9HDnkZ_Vgh4E1BzXFXrv=s900-c-k-c0x00ffffff-no-rj" class="h-24, w-24"></img>
 				</div>
 				<div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
-					<span className="text-xs tracking-wider uppercase dark:text-violet-600">Step 2 - Explicabo</span>
-					<span className="text-xl font-bold md:text-2xl">Iure incidunt labore</span>
+					<span className="text-xs tracking-wider uppercase dark:text-violet-600">June 2024 - July 2024</span>
+					<span className="text-xl font-bold md:text-2xl">SDE Intern at CyfrOne</span>
 					<span className="mt-4 dark:text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
 				</div>
 			</div>
@@ -226,4 +219,4 @@ function Header() {
     );
 }
 
-export default Header;
+export default Work;
